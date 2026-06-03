6c1fb507eff1a7742f2194a085015f94217f42f6
James Ross <james@flyingrobots.dev>
2026-06-02 18:02:01 -0700

    Fix: remove Svelte transpile-only config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,11 +5,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [
     svelte({
-      preprocess: sveltePreprocess({
-        typescript: {
-          transpileOnly: true,
-        },
-      }),
+      preprocess: sveltePreprocess(),
     }),
   ],
   server: {
